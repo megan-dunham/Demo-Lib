348ce268c60a632f87b9abadc1272a1a56f4ac06
megan-dunham <megan.dunham.2023@gmail.com>
2023-06-12 09:43:17 +0530

    - Publish Lib

---
+++ DemoLib/src/main/java/com/example/demolib/MyClass.java
@@ -0,0 +1,10 @@
+package com.example.demolib;
+
+public class MyClass {
+
+    public static void showToast(String message){
+        //Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+        System.out.println("This is a ShowToast function");
+    }
+}
--- app/src/main/java/com/example/demolib/MainActivity.java
+++ app/src/main/java/com/example/demolib/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.mylibrary.ToasterMessage;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
